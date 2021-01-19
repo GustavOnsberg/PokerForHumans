ff322932506b10018cd9c70a10623b00f17f5508
gustavonsberg <gustavonsberg@gmail.com>
2021-01-19 12:42:41 +0100

    added servercard.java

--- src/server/PokerServer.java
+++ src/server/PokerServer.java
@@ -327,17 +327,6 @@ class ServerPlayer{
     }
 }
 
-class ServerCard{
-    int id;
-    int suit;
-    int rank;
-    ServerCard(int id){
-        this.id = id;
-        suit = id % 4;
-        rank = id % 13;
-    }
-}
-
 
 
 
