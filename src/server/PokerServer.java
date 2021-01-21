d670a659d7554cb130e438b842dad84177bc6924
gustavonsberg <gustavonsberg@gmail.com>
2021-01-21 12:56:19 +0100

    ergerrthrthfefefefefefefeeefefefwgggdwd

--- src/server/PokerServer.java
+++ src/server/PokerServer.java
@@ -11,10 +11,6 @@ import java.util.Random;
 public class PokerServer {
     SpaceRepository serverRep;
 
-    ArrayList<Player> players = new ArrayList<Player>();
-    int turn;
-
-    int requiredBet = 0;
     int lastServerId = 0;
 
 
@@ -122,6 +118,7 @@ class Waiter implements Runnable{
 
             try {
                 Object[] tuple = serverRep.get(table).get(new FormalField(String.class),new FormalField(String.class),new FormalField(Integer.class));
+                System.out.println("Waiter ("+table+")> Reading: "+tuple[0].toString()+" : "+tuple[1].toString() + " : "+(int)tuple[2]);
                 if(tuple[0].equals("req") && !gameIsInProgress){
                     players.add(new ServerPlayer(tuple[1].toString()));
                     System.out.println("Waiter ("+table+")> New player has joined: "+tuple[1].toString());
@@ -150,7 +147,7 @@ class Waiter implements Runnable{
                                 broadcast("start_money","start_money",startMoney,startMoney,"Start money is "+startMoney);
                             }
 
-
+                            players.get(players.size()-1).isDealer = true;
                             startNewRound();
 
                             for(int i = 0; i < players.size(); i++){
@@ -164,46 +161,46 @@ class Waiter implements Runnable{
                         if(tuple[0].equals("fold") && tuple[1].equals(players.get(turn).uid)){
                             players.get(turn).state = 0;
                             broadcast("fold","fold",turn,turn,"Player "+turn+" has folded");
-                            turn = getNextPlayer();
                             System.out.println("Waiter ("+table+")> "+players.get(turn).name+" has folded");
                             broadcast("turn","turn",turn,turn,"It's player "+turn+"'s turn");
+                            turn = getNextPlayer();
                         }
                         else if(tuple[0].equals("call") && tuple[1].equals(players.get(turn).uid)){
-                            if(players.get(turn).bank + players.get(turn).bet >= server.requiredBet){
-                                players.get(turn).bank -= server.requiredBet - players.get(turn).bet;
-                                players.get(turn).bet = server.requiredBet;
+                            if(players.get(turn).bank + players.get(turn).bet >= requiredBet){
+                                players.get(turn).bank -= requiredBet - players.get(turn).bet;
+                                players.get(turn).bet = requiredBet;
                                 players.get(turn).state = 2;
                                 broadcast("call","call",requiredBet,turn,"Player "+turn+" has called "+requiredBet);
-                                turn = getNextPlayer();
                                 System.out.println("Waiter ("+table+")> "+players.get(turn).name+" has called "+requiredBet);
+                                turn = getNextPlayer();
                             }
                             else{
                                 players.get(turn).bet += players.get(turn).bank;
                                 players.get(turn).bank = 0;
                                 players.get(turn).state = 3;
                                 broadcast("allin","allin",players.get(turn).bet,turn,"Player "+turn+" is all in with "+players.get(turn).bet);
-                                turn = getNextPlayer();
                                 System.out.println("Waiter ("+table+")> "+players.get(turn).name+" is all in with "+players.get(turn).bet);
+                                turn = getNextPlayer();
                             }
                             broadcast("turn","turn",turn,turn,"It's player "+turn+"'s turn");
                         }
                         else if(tuple[0].equals("raise") && tuple[1].equals(players.get(turn).uid)){
-                            if((int)tuple[2] + server.requiredBet <= players.get(turn).bet + players.get(turn).bank){
-                                players.get(turn).bank -= (int)tuple[2] + server.requiredBet - players.get(turn).bet;
-                                players.get(turn).bet = (int)tuple[2] + server.requiredBet;
-                                server.requiredBet += (int)tuple[2];
+                            if((int)tuple[2] + requiredBet <= players.get(turn).bet + players.get(turn).bank){
+                                players.get(turn).bank -= (int)tuple[2] + requiredBet - players.get(turn).bet;
+                                players.get(turn).bet = (int)tuple[2] + requiredBet;
+                                requiredBet += (int)tuple[2];
                                 players.get(turn).state = 2;
                                 broadcast("raise","raise",players.get(turn).bet,turn,"Player "+turn+" has raised to "+players.get(turn).bet);
-                                turn = getNextPlayer();
                                 System.out.println("Waiter ("+table+")> "+players.get(turn).name+" has raised by "+(int)tuple[2]);
+                                turn = getNextPlayer();
                             }
                             else{
                                 players.get(turn).bet += players.get(turn).bank;
                                 players.get(turn).bank = 0;
                                 players.get(turn).state = 3;
                                 broadcast("allin","allin",players.get(turn).bet,turn,"Player "+turn+" is all in with "+players.get(turn).bet);
-                                turn = getNextPlayer();
                                 System.out.println("Waiter ("+table+")> "+players.get(turn).name+" is all in with "+players.get(turn).bet);
+                                turn = getNextPlayer();
                             }
                             broadcast("turn","turn",turn,turn,"It's player "+turn+"'s turn");
                         }
@@ -356,6 +353,7 @@ class Waiter implements Runnable{
     }
 
     void broadcast(String str1, String str2,int i,int j, String print) throws InterruptedException {
+        System.out.println("Waiter ("+table+")> Broadcasting: "+str1+" : "+str2+" : "+i+" : "+j);
         for(ServerPlayer p : players){
             serverRep.get(table).put(p.uid, str1, str2, i,j, print);
         }
