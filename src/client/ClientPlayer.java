ccc8066b87d4e6baf24f188a6749f5ee9ee0c50a
gustavonsberg <gustavonsberg@gmail.com>
2021-01-21 13:09:47 +0100

    made ClientPlayer.java

---
+++ src/client/ClientPlayer.java
@@ -0,0 +1,43 @@
+package client;
+
+import java.util.ArrayList;
+
+public class ClientPlayer {
+    String uid;
+    String name;
+
+    public int bank;
+    ArrayList<ClientCard> cards = new ArrayList<>();
+    public int bet;
+
+    int score;
+
+    /*
+    -1 = out
+    0 = folded
+    1 = yet to bet
+    2 = in the game
+    3 = all in
+    4 = players turn
+
+    * */
+    int state;
+
+
+    boolean isDealer;
+    boolean isSmallBlind;
+    boolean isBigBlind;
+
+    ClientPlayer() {
+
+    }
+
+    public int getBank() {
+        return bank;
+    }
+
+    public int getState() {
+        return state;
+    }
+
+}
--- src/client/PokerClient.java
+++ src/client/PokerClient.java
@@ -125,47 +125,6 @@ public class PokerClient implements Runnable{
 }
 
 
-class ClientPlayer{
-    String uid;
-    String name;
-
-    public int bank;
-    ArrayList<ClientCard> cards = new ArrayList<>();
-    public int bet;
-
-    int score;
-
-    /*
-    -1 = out
-    0 = folded
-    1 = yet to bet
-    2 = in the game
-    3 = all in
-    4 = players turn
-
-    * */
-    int state;
-
-
-
-    boolean isDealer;
-    boolean isSmallBlind;
-    boolean isBigBlind;
-
-    ClientPlayer(){
-
-    }
-
-    public int getBank() {
-        return bank;
-    }
-
-    public int getState() {
-        return state;
-    }
-
-}
-
 class ClientCard{
     int id;
     int suit;
